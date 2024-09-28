232bfebeeeaae1fb844085ee118101d651ad0233
= <=>
2024-09-28 16:47:40 -0400

    added shell script

---
+++ set_env_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export SHOPPING="localhost:7770"
+export SHOPPING_ADMIN="localhost:7780/admin"
+export REDDIT="localhost:9999"
+export GITLAB="localhost:8023"
+export MAP="localhost:3000"
+export WIKIPEDIA="localhost:8888/wikipedia_en_all_maxi_2022-05/A/User:The_other_Kiwix_guy/Landing"
+export HOMEPAGE="localhost:4399"
+echo "Environment variables have been set successfully."
